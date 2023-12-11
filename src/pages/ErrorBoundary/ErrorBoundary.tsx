5ce7d5016467d82293719c3e435fc2f059d22fef
amberlynn <amberlynn364@gmail.com>
2023-12-11 20:20:33 +0300

    feat: add logic to NotFoundPage and ErrorBoundary

--- src/pages/ErrorBoundary/ErrorBoundary.tsx
+++ src/pages/ErrorBoundary/ErrorBoundary.tsx
@@ -1,3 +1,16 @@
+import { Link, useRouteError } from 'react-router-dom';
+import RouterPath from '../../router/routerTypes';
+import styles from './ErrorBoundary.module.css';
+
 export default function ErrorBoundary() {
-  return <div>ErrorBoundary</div>;
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div className={styles.errorWrapper}>
+      <img src="/error-img.svg" className={styles.errorImg} alt="error-img" />
+      <h2>Whoops, Something went wrong</h2>
+      <Link to={RouterPath.Main}>Go To Main Page!</Link>;
+    </div>
+  );
 }
--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,3 +1,13 @@
+import { Link } from 'react-router-dom';
+import RouterPath from '../../router/routerTypes';
+import styles from './NotFoundPage.module.css';
+
 export default function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  return (
+    <div className={styles.errorWrapper}>
+      <img src="error-img.svg" className={styles.errorImg} alt="error-img" />
+      <h2>Whoops, page does not exist</h2>
+      <Link to={RouterPath.Main}>Go To Main Page!</Link>;
+    </div>
+  );
 }
