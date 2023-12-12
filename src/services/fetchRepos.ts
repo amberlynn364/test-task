5ceaf4c9090935f060509ef51c8795fb2bd91536
amberlynn <amberlynn364@gmail.com>
2023-12-13 00:29:28 +0300

    feat: implement fetchRepos service

---
+++ src/services/fetchRepos.ts
@@ -0,0 +1,22 @@
+import { ApiResponse } from '../types';
+
+export default async function fetchRepos(
+  value: string,
+  signal: AbortSignal
+): Promise<ApiResponse> {
+  try {
+    const response = await fetch(
+      `https://api.github.com/search/repositories?q=${value}&per_page=10`,
+      { signal }
+    );
+    const data: ApiResponse = await response.json();
+    return data;
+  } catch (error) {
+    if (error instanceof Error && error.name === 'AbortError') {
+      console.error('Interrupted!');
+    } else {
+      console.error(error);
+    }
+    throw error;
+  }
+}
