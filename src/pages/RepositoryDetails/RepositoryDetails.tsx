a0305a156239bd066464eda45dbf6967597ff543
amberlynn <amberlynn364@gmail.com>
2023-12-13 22:47:54 +0300

    feat: add logic to RepositoryDetails page

--- src/pages/RepositoryDetails/RepositoryDetails.tsx
+++ src/pages/RepositoryDetails/RepositoryDetails.tsx
@@ -1,3 +1,17 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import reposContainer from '../../store/ReposContainer';
+import { RepositoryData } from '../../types';
+import RepoDetailedCard from '../../components/RepoDetailedCard/RepoDetailedCard';
+
 export default function RepositoryDetails() {
-  return <div>RepositoryDetails</div>;
+  const { id } = useParams();
+  const { repos } = reposContainer;
+  const [repo, setRepo] = useState<RepositoryData | null>(null);
+  useEffect(() => {
+    const data = repos.find((item) => item.id === Number(id));
+    if (data) setRepo(data);
+  }, [id, repos]);
+  if (!repo) return <p>Loading...</p>;
+  return <RepoDetailedCard repo={repo} />;
 }
