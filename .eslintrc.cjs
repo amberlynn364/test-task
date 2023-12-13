0eaf63f52a432bb67eb299322f79a568cf9329ff
amberlynn <amberlynn364@gmail.com>
2023-12-14 00:39:20 +0300

    refactor: made minor changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -32,5 +32,10 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': 0,
     'no-console': ['warn', { allow: ['error'] }],
+    '@typescript-eslint/naming-convention': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+    'react/prop-types': 0,
+    'react/jsx-props-no-spreading': 0,
   },
 };
