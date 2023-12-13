699fbe65f0c3da52842d091cef4e130afe5f8483
amberlynn <amberlynn364@gmail.com>
2023-12-14 00:37:40 +0300

    refactor: add search params for fetch in Search Section

--- src/components/SearchSection/SearchSection.tsx
+++ src/components/SearchSection/SearchSection.tsx
@@ -1,14 +1,20 @@
 import { useEffect, useState } from 'react';
 import { observer } from 'mobx-react-lite';
+import { useSearchParams } from 'react-router-dom';
 import TextField from '../View/TextField/TextField';
 import ClipBoard from '../ClipBoard/ClipBoard';
 import reposContainer from '../../store/ReposContainer';
 import fetchRepos from '../../services/fetchRepos';
 import useThrottledCallback from '../../hooks/useThrottledCallback';
+import reposCounter from '../../store/ReposCounter';
+import styles from './SearchSection.module.css';
 
 const SearchSection = observer(() => {
   const [searchValue, setSearchValue] = useState('');
   const { setRepo } = reposContainer;
+  const { setReposCount } = reposCounter;
+  const [searchParams] = useSearchParams();
+  const currentPage = searchParams.get('page') || '1';
 
   const handleUpdateSearchValue = useThrottledCallback(
     (value: string): void => {
@@ -20,14 +26,17 @@ const SearchSection = observer(() => {
   useEffect(() => {
     const abortController = new AbortController();
     const { signal } = abortController;
-    fetchRepos(searchValue, signal).then((data) => {
-      if (data) setRepo(data.items);
+    fetchRepos(searchValue, currentPage, signal).then((data) => {
+      if (data) {
+        setRepo(data.items);
+        setReposCount(data.total_count);
+      }
     });
 
     return () => abortController.abort();
-  }, [searchValue, setRepo]);
+  }, [searchValue, setRepo, currentPage, setReposCount]);
   return (
-    <section>
+    <section className={styles.searchSection}>
       <TextField
         label="Search Input"
         name="searchInput"
